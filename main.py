47754d8cb5e420c2abb2b39dbb5a53c227fe1752
Rishi Kumar <rishikumar0897@gmail.com>
2024-09-08 23:04:51 -0400

    third commit

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ y = data['median_house_value']
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 train_data = x_train.join(y_train)
 
-train_data['total_rooms'] = np.log(train_data['total_rooms'])
-train_data['total_bedrooms'] = np.log(train_data['total_bedrooms'])
-train_data['population'] = np.log(train_data['population'])
-train_data['households'] = np.log(train_data['households'])
+train_data['total_rooms'] = np.log(train_data['total_rooms'] + 1)
+train_data['total_bedrooms'] = np.log(train_data['total_bedrooms'] + 1)
+train_data['population'] = np.log(train_data['population'] + 1)
+train_data['households'] = np.log(train_data['households'] + 1)
 
 train_data = train_data.join(pd.get_dummies(train_data.ocean_proximity)).drop(["ocean_proximity"], axis=1)
 
@@ -33,18 +33,18 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
 
 scaler = StandardScaler()
+
 x_train, y_train = train_data.drop(['median_house_value'], axis=1), train_data['median_house_value']
 x_train_s = scaler.fit_transform(x_train)
-
 regression = LinearRegression()
-regression.fit(x_train, y_train)
+regression.fit(x_train_s, y_train)
 
 test_data = x_test.join(y_test)
 
-test_data['total_rooms'] = np.log(test_data['total_rooms'])
-test_data['total_bedrooms'] = np.log(test_data['total_bedrooms'])
-test_data['population'] = np.log(test_data['population'])
-test_data['households'] = np.log(test_data['households'])
+test_data['total_rooms'] = np.log(test_data['total_rooms'] + 1)
+test_data['total_bedrooms'] = np.log(test_data['total_bedrooms'] + 1)
+test_data['population'] = np.log(test_data['population'] + 1)
+test_data['households'] = np.log(test_data['households'] + 1)
 
 test_data = test_data.join(pd.get_dummies(test_data.ocean_proximity)).drop(["ocean_proximity"], axis=1)
 
@@ -52,5 +52,6 @@ test_data['bedroom_ratio'] = test_data['total_bedrooms'] / test_data['total_room
 test_data['household_rooms'] = test_data['total_rooms'] / test_data['households']
 
 x_test, y_test = test_data.drop(['median_house_value'], axis=1), test_data['median_house_value']
-x_test_s = scaler.fit_transform(x_train)
+x_test_s = scaler.transform(x_test)
 regression.score(x_test_s, y_test)
+
